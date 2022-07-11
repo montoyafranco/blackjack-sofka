4269f976e10e5c48c8543858ac9bb8693484383b
montoyafranco <francomontoya9810@gmail.com>
2022-07-11 20:47:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+class Game{
+    //Atributtes 
+ ///  
+ /*
+- Se gana si uno esta entre (18 y 21)
+- Necesito 1 contador de rondas 
+- Necesito  un objeto que contenga cartas
+
+
+
+
+                      CONDICION GANADORA           if(puntaje_actual >= 18 || puntaje_actual >= 21 )
+
+                      CONDICION DE "A"
+
+
+
+puntaje_actual = 0
+
+contador_rondas = 1
+
+array_cartas = [A,2,3,4,5,6,7,8,9,10,K,J,Q]
+
+array_tipo_carta = [♠, ♥ ,🔹 ,🍀 ]
+
+
+
+
+
+ */    
+
+    //constructors
+    // methods
+
+}
+
