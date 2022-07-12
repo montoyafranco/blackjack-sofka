1d221c5896c59f0b5248960893a42f1032440697
montoyafranco <francomontoya9810@gmail.com>
2022-07-12 17:12:14 -0300

    logic working -creating switch - later make the OOP

--- index.js
+++ index.js
@@ -67,35 +67,93 @@ array_tipo_carta = [♠, ♥ ,🔹 ,🍀 ]
 // });
 let elegidas = []
 let sumatoria = 0
+let ronda = 1
+let answer ="";
 
-function Obtener_Random(){
-const i_firstCard=Math.floor(Math.random() * Deck.length);
-const i_secondCard=Math.floor(Math.random() * Deck.length);
- elegidas.push(Deck[i_firstCard])
- elegidas.push(Deck[i_secondCard])
- Deck.splice(i_firstCard, 1)
+// EMPIEZO LA RONDA 2 cartas random
+function startRound(){
+const i_firstCard=Math.floor(Math.random() * Deck.length); // guardo el indice random con el length del DECK
+
+elegidas.push(Deck[i_firstCard]) // pusheo a cartas elegidas con el indice
+
+Deck.splice(i_firstCard, 1) // la saco para que no se repita del array
+
+const i_secondCard=Math.floor(Math.random() * Deck.length); 
+ elegidas.push(Deck[i_secondCard]) 
  Deck.splice(i_secondCard,1)
 
-
-
 }
 
+// pushear 1 carta mas random
+function askCard(){
+    elegidas.push(Deck[Math.floor(Math.random() * Deck.length)])
+    
+}
 
-Obtener_Random()
-console.log ("Soy el console log " ,elegidas)
-console.log(Deck.length)
+let bandera_A = 0 
+startRound()
 
-for (let i = 0 ;i < elegidas.length;i++ ){
+revisar_puntos()
 
-    if(typeof(elegidas[i].card) == "string" && elegidas[i].card != "A" ){
-        sumatoria = sumatoria + 10
-        console.log()
 
-    }else{
-        sumatoria = sumatoria + elegidas[i].card
+console.log ("Estas son tus cartas " ,elegidas)
+
+
+ // DETECTAR CANTIDAD DE A 
+
+// ciclo para corroborar tipo de carta y puntaje
+function revisar_puntos(){
+for (let i = 0 ;i < elegidas.length;i++ ){  
+
+if(elegidas[i].card == "K" || elegidas[i].card == "Q" || elegidas[i].card == "J" ){
+    sumatoria = sumatoria + 10
+}else if (typeof(elegidas[i].card) == "number" ){
+    sumatoria = sumatoria + elegidas[i].card
+
+}else if( elegidas[i].card == "A" && bandera_A == 0)
+{ sumatoria = sumatoria + 11
+    bandera_A = bandera_A + 1
+}else{
+    sumatoria = sumatoria + 1
+}
+    
+}}
+
+console.log("Tu puntaje es : " ,sumatoria)
+if(sumatoria >= 18 && sumatoria <=21 ){console.log("GANASTE")}else{console.log("PERDISTE")}
+
+// si todavia te falta para llegar a 21 con Y sacas otra carta con N te retiras y ganas lo acumulado 
+//si ganas se te pregunta con R si queres otra ronda para mas premio 
+
+import readline from 'readline';
+
+
+const scanner = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+  scanner.question(" Y - Draw the card...\n N - Finish the game...\n", answer => {
+    switch(answer){
+              case "Y":
+                console.log("Draw the card...")
+                askCard();
+                sumatoria = 0;
+                revisar_puntos()
+                console.log ("Estas son tus cartas " ,elegidas)
+                console.log("Tu puntaje es : " ,sumatoria)
+                if(sumatoria >= 18 && sumatoria <=21 ){console.log("GANASTE")}else{console.log("PERDISTE")}
+
+                break;
+
+
+              case "N":
+                console.log("Ok, game finished.");
+                break;
+              default:
+                console.log("Invalid input. ");
+                break;
+    
     }
-
-
-}
-console.log(sumatoria)
-console.log(elegidas[0].card)
+    scanner.close();
+});
