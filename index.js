eda5cfe9f58c57c0f361927d0d1d9bca3ff8e45f
montoyafranco <francomontoya9810@gmail.com>
2022-07-11 21:01:10 -0300

    More Pseudo-code

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 class Game{
-    //Atributtes 
- ///  
- /*
+ 
+/*
+ ------------- Requisitos
+
 - Se gana si uno esta entre (18 y 21)
 - Necesito 1 contador de rondas 
 - Necesito  un objeto que contenga cartas
 
+- Necesito por comandos que con Y o N continue jugando o no
 
+-----------IDEAS----
+
+Meter todo dentro de un menu de opciones con ciclo while 
+
+
+
+----------- Pseudo-logica
 
 
                       CONDICION GANADORA           if(puntaje_actual >= 18 || puntaje_actual >= 21 )
 
-                      CONDICION DE "A"
+                      CONDICION DE "A"             si aparece 1 vez == 11  else 1  
+
+                      CONDICION RONDAS              R1 == WIN ? 1000
+                                                    R2 == WIN ? 2000 : 0 
+                                                    R3 == WIN ? 3000 : 2000
+
+                      CONDICION CARTAS              No debe repetirse 
 
 
-
+------------- VARIABLES
 puntaje_actual = 0
 
 contador_rondas = 1
@@ -22,15 +37,8 @@ contador_rondas = 1
 array_cartas = [A,2,3,4,5,6,7,8,9,10,K,J,Q]
 
 array_tipo_carta = [♠, ♥ ,🔹 ,🍀 ]
+ */
 
-
-
-
-
- */    
-
-    //constructors
-    // methods
 
 }
 
